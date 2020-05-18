c2b9cabb3b21031552dce94fbd737d82f89dee39
shadownxr <shadonwxr1@gmail.com>
2020-05-18 21:03:04 +0200

    tableki

--- src/components/mainmenu/DaneTechniczne.js
+++ src/components/mainmenu/DaneTechniczne.js
@@ -1,15 +1,81 @@
 import React from 'react';
 import './DaneTechniczne.css';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+});
 
 const DaneTechniczne = ({ carData }) => (
-    <div className="DaneTechniczne">
-        Silnik <br/>
-        Typ: {carData.technicalities.engine.type}<br/>
-        Moc: {carData.technicalities.engine.power}<br/>
-        Wymiary <br/>
-        Długość: {carData.technicalities.size.length}<br/>
-        Szerokość: {carData.technicalities.size.width}<br/>
-    </div>
+    <TableContainer component={Paper}>
+      <Table className={useStyles().table} aria-label="simple table">
+        <TableHead>
+          <StyledTableRow>
+            <StyledTableCell align="center" colSpan={2}>Silnik</StyledTableCell>
+          </StyledTableRow>
+        </TableHead>
+        <TableBody>
+            <StyledTableRow>
+              <StyledTableCell component="th" scope="row">
+              Typ:
+              </StyledTableCell>
+              <StyledTableCell align="right">{carData.technicalities.engine.type}</StyledTableCell>   
+            </StyledTableRow>
+            <StyledTableRow>
+              <StyledTableCell component="th" scope="row">
+              Moc:
+              </StyledTableCell>
+              <StyledTableCell align="right">{carData.technicalities.engine.power}</StyledTableCell>   
+            </StyledTableRow>
+        </TableBody>
+        <TableHead>
+          <StyledTableRow>
+            <StyledTableCell align="center" colSpan={2}>Wymiary</StyledTableCell>
+          </StyledTableRow>
+        </TableHead>
+        <TableBody>
+            <StyledTableRow>
+              <StyledTableCell component="th" scope="row">
+              Długość:
+              </StyledTableCell>
+              <StyledTableCell align="right">{carData.technicalities.size.length}</StyledTableCell>   
+            </StyledTableRow>
+            <StyledTableRow>
+              <StyledTableCell component="th" scope="row">
+              Szerokość:
+              </StyledTableCell>
+              <StyledTableCell align="right">{carData.technicalities.size.width}</StyledTableCell>   
+            </StyledTableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
 );
 
 export default DaneTechniczne
--- src/components/mainmenu/Serwisy.js
+++ src/components/mainmenu/Serwisy.js
@@ -1,5 +1,38 @@
 import React from 'react';
 import './Serwisy.css';
+import Button from '@material-ui/core/Button'
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+}))(TableCell);
+  
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+}))(TableRow);
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+});
 
 class Serwisy extends React.Component {
     constructor(){
@@ -11,8 +44,34 @@ class Serwisy extends React.Component {
 
 
     render(){
-        const serviceList = this.props.carData.services.map((service,i) => 
+        const serviceList = this.props.carData.services.map((service,i) =>
+        <TableBody> 
+            <StyledTableRow key={i} className = "serviceList">
+                <TableCell>{service.date}</TableCell>
+                <TableCell align="right">{service.title}</TableCell>
+            </StyledTableRow>
+        </TableBody>
+        )
+
+        return(
+            <TableContainer className="Serwisy">
+                <Table className={useStyles().table} aria-label="simple table">
+                <TableHead>
+                    <StyledTableRow>
+                        <StyledTableCell align="center" colSpan={2}>Ostatnie serwisy</StyledTableCell>
+                    </StyledTableRow>
+                </TableHead>
+                    {serviceList}
+                </Table>
+            </TableContainer>
+        )
+    }
+}
+
+/*
+const serviceList = this.props.carData.services.map((service,i) => 
             <div key={i} className = "serviceList">
+                <table>
                     <tbody>
                         <tr>
                             <td>Data: </td><td>{service.date}</td>
@@ -21,18 +80,18 @@ class Serwisy extends React.Component {
                             <td>Tytuł: </td><td>{service.title} </td>
                         </tr>
                     </tbody>
+                </table>
             </div>
         )
 
         return(
             <div className="Serwisy">
-                Ostatnie Serwisy
                 <table>
+                    <thead><tr><td>Ostatnie Serwisy <Button>Dodaj</Button></td></tr></thead>
                     {serviceList}
                 </table>
             </div>
         )
-    }
-}
+*/
 
 export default Serwisy
